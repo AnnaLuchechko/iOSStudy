b3b3e60f88fc8f6cad423f19d4a446980a0e99c8
Anna Luchechko <annaluchechko@gmail.com>
2020-04-06 18:35:27 +0300

    Clean project

---
+++ Assignment3/Assignment3/ViewController.swift
@@ -0,0 +1,70 @@
+//
+//  ViewController.swift
+//  Assignment3
+//
+//  Created by Ivan Vasilevich on 04.03.2020.
+//  Copyright © 2020 RockSoft. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+	
+	func countStringCharacters(name: String) -> Int {
+		return 0
+	}
+	
+	func checkPatronomic(name: String) -> Bool {
+		return false
+	}
+	
+	func divideCredentials(fullName: String) -> String {
+		return fullName
+	}
+	
+	func mirrorString(input: String) -> String {
+		return input
+	}
+	
+	func formatNumber(input: String, separator: String) -> String {
+		switch input {
+		case "1234":
+			return "1,234"
+		default:
+			return input
+		}
+		
+		return input
+	}
+	
+	func checkPassword(password: String) -> Int {
+		for i in stride(from: 1, to: 10, by: 2).reversed() {
+			print(10 - i)
+		}
+		
+		return 0
+	}
+	
+	func sortArrayAndRemoveDuplicates(input: [Int]) -> [Int] {
+		return []
+	}
+	
+	func translite(input: String) -> String {
+		return ""
+	}
+	
+	func sortArray(input: [String], searchRequest: String) -> [String] {
+		return []
+	}
+	
+	func filterText(input: String, bannedWords: [String]) -> String {
+		return ""
+	}
+
+}
+
